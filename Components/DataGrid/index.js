e9ce32e569c02eb12e064f82325a3bc7a6fb90b4
Renan Nardi <renan_terra_rica@hotmail.com.br>
2023-02-04 14:27:00 -0300

    DataGrid

--- Components/DataGrid/index.js
+++ Components/DataGrid/index.js
@@ -6,14 +6,23 @@ export default function BasicExampleDataGrid({ data }) {
   return (
     <div style={{ marginTop: 70, height: 400, width: 530 }}>
       <DataGrid
+        sx={{ backgroundColor: "#ccc", borderColor: "#ccc", borderWidth: 5, borderStyle: "solid" }}
         {...data}
-        onCellClick={
+        onCellDoubleClick={
           (params, event) => {
-            if (params.field === 'name') {
-              console.log('cell clicked', params);
-            }
+            console.log('cell clicked:',
+              "ID:", params.row.id,
+              "Age:", params.row.age,
+              "First name:", params.row.firstName,
+              "Full name:", params.row.fullName,
+              "Last name:", params.row.lastName
+            );
           }
         }
+        components={{
+          Toolbar: GridToolbar,
+          ToolbarColor: "inherit",
+        }}
       />
     </div>
   );
