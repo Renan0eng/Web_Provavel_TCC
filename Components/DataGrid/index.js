d7849ad8b13e9ab7ea773cfa3ba14ce652a8600b
Renan Nardi <renan_terra_rica@hotmail.com.br>
2023-02-04 13:44:35 -0300

    coldChambers error

--- Components/DataGrid/index.js
+++ Components/DataGrid/index.js
@@ -2,15 +2,24 @@ import * as React from 'react';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import { useDemoData } from '@mui/x-data-grid-generator';
 
-const VISIBLE_FIELDS = ['name', 'rating', 'country', 'dateCreated', 'isAdmin'];
 
 
 
 export default function BasicExampleDataGrid({ data }) {
 
   return (
-    <div style={{ marginTop: 70, height: 400, width: 530  }}>
-      <DataGrid {...data} components={{ Toolbar: GridToolbar }} />
+    <div style={{ marginTop: 70, height: 400, width: 530 }}>
+      <DataGrid
+        {...data}
+        onCellClick={
+          (params, event) => {
+            if (params.field === 'name') {
+              console.log('cell clicked', params);
+            }
+          }
+        }
+        on
+      />
     </div>
   );
 }
--- Components/coldChambers/index.js
+++ Components/coldChambers/index.js
@@ -1,7 +1,5 @@
-import DataGrid from '../../Components/DataGrid';
+import DataGrid from '../DataGrid';
 export default function coldChambers({ data }) {
-
-
   return (
     <div>
       <DataGrid data={data} />
