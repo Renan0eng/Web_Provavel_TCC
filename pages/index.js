f83c42e4154308b1ff4103eb4e93a2a1c5f1a318
Renan Nardi <renan_terra_rica@hotmail.com.br>
2023-02-04 16:06:49 -0300

    .

--- pages/index.js
+++ pages/index.js
@@ -236,13 +236,7 @@ export default function Home() {
               {...dataGrid}
               onCellDoubleClick={
                 (params, event) => {
-                  console.log('cell clicked:',
-                    "ID:", params.row.id,
-                    "Age:", params.row.age,
-                    "First name:", params.row.firstName,
-                    "Full name:", params.row.fullName,
-                    "Last name:", params.row.lastName
-                  );
+                  console.log('cell clicked:',params);
                 }
               }
             />
