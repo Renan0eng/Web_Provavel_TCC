20128ecde9e89e0287e00873921fc9d8de071e96
Renan Nardi <renan_terra_rica@hotmail.com.br>
2023-02-04 14:30:09 -0300

    DataGrid

--- Components/coldChambers/index.js
+++ Components/coldChambers/index.js
@@ -1,8 +1,6 @@
 import DataGrid from '../DataGrid';
 export default function coldChambers({ data }) {
   return (
-    <div>
-      <DataGrid data={data} />
-    </div>
+    <DataGrid data={data} />
   )
 }
