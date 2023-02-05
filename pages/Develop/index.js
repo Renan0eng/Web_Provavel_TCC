bbf650ced969c1db38154bb9ed5c30d3693e6066
Renan Nardi <renan_terra_rica@hotmail.com.br>
2023-02-05 00:07:39 -0300

    Revert "Revert "nav error""
    This reverts commit a0fd60dca05bde18117f5b73b3dc0264579da4a6.

--- pages/Develop/index.js
+++ pages/Develop/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 import Nav from "../../Components/Nav"
-
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Grid from "../../Components/DataGrid"
 
 export default function Home() {
+
+  const [render, setRender] = React.useState('dash')
 
   const [dataGrid, setDataGrid] = React.useState({
     columns: [
@@ -23,70 +24,62 @@ export default function Home() {
   const gridWidth = dataGrid.columns.reduce((acc, column) => acc + column.width, 50);
 
 
-  function renderDash() {
+  function renderDash(){
     return (
-      <Box sx={{pl: 8 }}>
-        <Box sx={{ height: 50, width: '100%' }}>
+      <Box sx={{pl:8}}>
+      <Box sx={{ height: 100, width: '100%' }}>
           <Typography variant="h6" noWrap align='center' component="div">
             Grupos de comtrole
           </Typography>
         </Box>
-        <Box sx={{ width: '100%', flexWrap: "wrap", display: "flex", justifyContent: 'space-around' }}>
-          {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth} />}
+        <Box sx={{ width: '100%', flexWrap: "wrap",display: "flex", justifyContent: 'space-around'}}>
+        {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth}/>}
 
-          <Box>
-            <Typography variant="h6" noWrap align='center' component="div">
-              MUSSUM
-              IPSUM
-            </Typography>
-            <Typography p={"4%"}>
-              Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus.Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo!Diuretics paradis num copo é motivis de denguis.Casamentiss faiz malandris se pirulitá.
+        <Box>
+          <Typography variant="h6" noWrap align='center' component="div">
+          MUSSUM
+          IPSUM
+          </Typography>
+          <Typography p={"4%"}>
+            Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus.Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo!Diuretics paradis num copo é motivis de denguis.Casamentiss faiz malandris se pirulitá.
 
-              Per aumento de cachacis, eu reclamis.In elementis mé pra quem é amistosis quis leo.Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.Manduma pindureta quium dia nois paga.
+            Per aumento de cachacis, eu reclamis.In elementis mé pra quem é amistosis quis leo.Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.Manduma pindureta quium dia nois paga.
 
-              Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.Paisis, filhis, espiritis santis.Suco de cevadiss deixa as pessoas mais interessantis.Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.
+            Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.Paisis, filhis, espiritis santis.Suco de cevadiss deixa as pessoas mais interessantis.Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.
 
-              Interagi no mé, cursus quis, vehicula ac nisi.Quem manda na minha terra sou euzis!Pra lá , depois divoltis porris, paradis.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.
+            Interagi no mé, cursus quis, vehicula ac nisi.Quem manda na minha terra sou euzis!Pra lá , depois divoltis porris, paradis.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.
 
-              Delegadis gente finis, bibendum egestas augue arcu ut est.Suco de cevadiss deixa as pessoas mais interessantis.Aenean aliquam molestie leo, vitae iaculis nisl.Copo furadis é disculpa de bebadis, arcu quam euismod magna.
+            Delegadis gente finis, bibendum egestas augue arcu ut est.Suco de cevadiss deixa as pessoas mais interessantis.Aenean aliquam molestie leo, vitae iaculis nisl.Copo furadis é disculpa de bebadis, arcu quam euismod magna.
 
-              Mé faiz elementum girarzis, nisi eros vermeio.Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.
+            Mé faiz elementum girarzis, nisi eros vermeio.Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.
 
-              Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Diuretics paradis num copo é motivis de denguis.Pra lá , depois divoltis porris, paradis.In elementis mé pra quem é amistosis quis leo.
+            Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Diuretics paradis num copo é motivis de denguis.Pra lá , depois divoltis porris, paradis.In elementis mé pra quem é amistosis quis leo.
 
-              Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.Copo furadis é disculpa de bebadis, arcu quam euismod magna.Leite de capivaris, leite de mula manquis sem cabeça.Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.
+            Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.Copo furadis é disculpa de bebadis, arcu quam euismod magna.Leite de capivaris, leite de mula manquis sem cabeça.Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.
 
-              Suco de cevadiss deixa as pessoas mais interessantis.In elementis mé pra quem é amistosis quis leo.Detraxit consequat et quo num tendi nada.Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.
+            Suco de cevadiss deixa as pessoas mais interessantis.In elementis mé pra quem é amistosis quis leo.Detraxit consequat et quo num tendi nada.Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.
 
-              Sapien in monti palavris qui num significa nadis i pareci latim.Copo furadis é disculpa de bebadis, arcu quam euismod magna.Suco de cevadiss deixa as pessoas mais interessantis.Quem num gosta di mim que vai caçá sua turmis!
-            </Typography>
+            Sapien in monti palavris qui num significa nadis i pareci latim.Copo furadis é disculpa de bebadis, arcu quam euismod magna.Suco de cevadiss deixa as pessoas mais interessantis.Quem num gosta di mim que vai caçá sua turmis!
+          </Typography>
 
-          </Box>
+        </Box>
 
-          {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth} />}
-          {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth} />}
-          {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth} />}
-          {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth} />}
-          {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth} />}
-          {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth} />}
+        {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth}/>}
+        {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth}/>}
+        {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth}/>}
+        {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth}/>}
+        {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth}/>}
+        {false ? null : <Grid dataGrid={dataGrid} gridWidth={gridWidth}/>}
         </Box>
       </Box>
     );
   }
-
-  function renderNav() {
-    return (
-      <>
-        <Nav />
-      </>
-    );
-  }
-
+ 
 
   return (
-    <div>
-      {renderNav()}
-      {renderDash()}
-    </div>
+      <div>
+        <Nav />
+        { render === "dash" ? renderDash() : null}
+      </div>
   );
 }
